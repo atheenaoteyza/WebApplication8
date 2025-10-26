3d0f0d347004b41d9290192356e5dffbaf02ff18
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-10-26 23:30:20 +0800

    feat: GetAllAuthorsAsync & AddBookToAuthorAsync Tasks

--- WebApplication8/Services/BookstoreService.cs
+++ WebApplication8/Services/BookstoreService.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication8.DTOs;
 using WebApplication8.Models;
 
 namespace WebApplication8.Services
@@ -10,7 +12,44 @@ namespace WebApplication8.Services
         {
             _context = context;
         }
+
+
+        public async Task<List<AuthorDTO>> GetAllAuthorsAsync()
+        {
+            return await _context.Authors
+             .Include(a => a.Books)
+             .Select(a => new AuthorDTO
+             {
+                 AuthorId = a.AuthorId,
+                 FirstName = a.FirstName,
+                 LastName = a.LastName,
+                 Books = a.Books.Select(ab => new BookDTO
+                 {
+                     ISBN = ab.ISBN,
+                     Title = ab.Title,
+                     Price = ab.Price,
+                     Discount = ab.Discount
+                 }).ToList()
+             })
+                 .ToListAsync();
+
+        }
+
+        public async Task AddBookToAuthorAsync(int authorId, int isbn)
+        {
+            var author = await _context.Authors.FindAsync(authorId);
+            var book = await _context.Books.FindAsync(isbn);
+
+            if (author == null || book == null)
+                throw new Exception("Author or book not found");
+
+            //Prevent duplicates
+            if (author.Books.Any(ab => ab.ISBN == isbn))
+                throw new Exception("Book already assigned to this authtor");
+
+            author.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
     }
-
-
 }
