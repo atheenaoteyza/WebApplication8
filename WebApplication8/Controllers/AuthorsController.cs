dc7ed66c224380ae400f67f3e127495bd2f4aebf
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-10-28 18:04:01 +0800

    refactor: add Services in AuthorsController &  Program.cs

--- WebApplication8/Controllers/AuthorsController.cs
+++ WebApplication8/Controllers/AuthorsController.cs
@@ -2,50 +2,61 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication8.DTOs;
 using WebApplication8.Models;
+using WebApplication8.Service;
 
 namespace WebApplication8.Controllers
 {
     public class AuthorsController : Controller
     {
-        private readonly BookstoreContext _context;
+        private readonly BookstoreService _bookstoreService;
 
-        public AuthorsController(BookstoreContext context)
+        public AuthorsController(BookstoreService bookstoreService)
         {
-            _context = context;
+            _bookstoreService = bookstoreService;
         }
+
+        //   👇 Test endpoint to view authors + books
+        [HttpGet("/test-authors")]
+        public async Task<IActionResult> TestAuthors()
+        {
+            var authors = await _bookstoreService.GetAllAuthorsAsync();
+
+            return Ok(authors);
+        }
+
 
         // 👇 Test endpoint to view authors + books
-        [HttpGet("/test-authors")]
-        public IActionResult TestAuthors()
-        {
-            var authors = _context.Authors
-                .Include(a => a.Books)
-                .Select(a => new AuthorDTO
-                {
-                    AuthorId = a.AuthorId,
-                    FirstName = a.FirstName,
-                    LastName = a.LastName,
-                    Books = a.Books.Select(ab => new BookDTO
-                    {
-                        ISBN = ab.ISBN,
-                        Title = ab.Title,
-                        Price = ab.Price,
-                        Discount = ab.Discount
-                    }).ToList()
-                })
-                .ToList();
+        // [HttpGet("/test-authors")]
+        // public IActionResult TestAuthors()
+        // {
+        //     var authors = _context.Authors
+        //         .Include(a => a.Books)
+        //         .Select(a => new AuthorDTO
+        //         {
+        //             AuthorId = a.AuthorId,
+        //             FirstName = a.FirstName,
+        //             LastName = a.LastName,
+        //             Books = a.Books.Select(ab => new BookDTO
+        //             {
+        //                 ISBN = ab.ISBN,
+        //                 Title = ab.Title,
+        //                 Price = ab.Price,
+        //                 Discount = ab.Discount
+        //             }).ToList()
+        //         })
+        //         .ToList();
 
-            foreach (var author in authors)
-            {
-                Console.WriteLine($"{author.FirstName} {author.LastName}");
-                foreach (var book in author.Books)
-                {
-                    Console.WriteLine($"  - {book.Title}");
-                }
-            }
+        //     foreach (var author in authors)
+        //     {
+        //         Console.WriteLine($"{author.FirstName} {author.LastName}");
+        //         foreach (var book in author.Books)
+        //         {
+        //             Console.WriteLine($"  - {book.Title}");
+        //         }
+        //     }
 
-            // You can also return JSON if you want to see results in the browser:
-            return Json(authors);
-        }
+        //     // You can also return JSON if you want to see results in the browser:
+        //     return Json(authors);
+        // }
     }
 }
